6496c84aa757038a7e45afc46c469ef3c4058c4b
marijntepas <marijntepas@gmail.com>
2022-08-16 21:05:23 +0200

    Tag menu now works with buttons

--- library/cogs/tag.py
+++ library/cogs/tag.py
@@ -9,7 +9,7 @@ COG_NAME = "tag"
 class Tag(Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-    
+
     @app_commands.command(
         name=COG_NAME,
         description = "Add or toggle tags on yourself to keep track of what you are currently working on."
@@ -30,7 +30,14 @@ class Tag(Cog):
             await self.show_tag_menu(interaction, userID, f"These are your tags. Green tags are active, red tags are not. Click the tag name to toggle it, and the X to delete a tag.")
         elif not re.match(r"^[a-zA-Z0-9_]*$", tag_name):
             await interaction.response.send_message( f"Tag names may only include alphanumeric characters and underscores. Such as example_tag_2", ephemeral=True)
-        elif self.bot.user_manager.has_tag_active(userID, tag_name):
+        elif self.bot.user_manager.has_tag(userID, tag_name):
+            await self.toggle_tag(interaction, userID, tag_name)
+        else:
+            self.bot.user_manager.add_tag_active(userID, tag_name)
+            await self.show_tag_menu(interaction, userID, f"The tag " + tag_name + " has been added to your user and turned on.")
+
+    async def toggle_tag(self, interaction, userID, tag_name):
+        if self.bot.user_manager.has_tag_active(userID, tag_name):
             self.bot.user_manager.remove_tag(userID, tag_name)
             self.bot.user_manager.add_tag_inactive(userID, tag_name)
             await self.show_tag_menu(interaction, userID, f"The tag " + tag_name + " has been turned off.")
@@ -38,24 +45,22 @@ class Tag(Cog):
             self.bot.user_manager.remove_tag(userID, tag_name)
             self.bot.user_manager.add_tag_active(userID, tag_name)
             await self.show_tag_menu(interaction, userID, f"The tag " + tag_name + " has been turned on.")
-        else:
-            self.bot.user_manager.add_tag_active(userID, tag_name)
-            await self.show_tag_menu(interaction, userID, f"The tag " + tag_name + " has been added to your user and turned on.")
 
     async def show_tag_menu(self, interaction, userID, description):
         activeTags = self.bot.user_manager.get_tags_active(userID)
         tags = sorted(activeTags + self.bot.user_manager.get_tags_inactive(userID), key=str.lower)
-
         view = View()
         i = 0
         for tag in tags:
             if tag != "":
                 style=discord.ButtonStyle.red
                 if tag in activeTags: style=discord.ButtonStyle.green
-                button = Button(label=tag, style=style, row=i)
-                view.add_item(button)
-                button2 = Button(label="X", style=style, row=i)
-                view.add_item(button2)
+
+                button_toggle = Toggle_button(self, tag, style, i, tag)
+                button_delete = Delete_button(self, "X", style, i, tag)
+
+                view.add_item(button_toggle)
+                view.add_item(button_delete)
                 i += 1
 
         await interaction.response.send_message(description, view=view, ephemeral=True)
@@ -67,3 +72,24 @@ class Tag(Cog):
 
 async def setup(bot) -> None:
     await bot.add_cog(Tag(bot))
+    
+class Toggle_button(Button):
+    def __init__(self, cog, label, style, row, tag_name):
+        super().__init__(label=label, style=style, row=row)
+        self.cog = cog
+        self.bot = cog.bot
+        self.tag_name = tag_name
+    async def callback(self, interaction):
+        userID = self.bot.user_manager.get_user_id(interaction.user)
+        await self.cog.toggle_tag(interaction, userID, self.tag_name)
+
+class Delete_button(Button):
+    def __init__(self, cog, label, style, row, tag_name):
+        super().__init__(label=label, style=style, row=row)
+        self.cog = cog
+        self.bot = cog.bot
+        self.tag_name = tag_name
+    async def callback(self, interaction):
+        userID = self.bot.user_manager.get_user_id(interaction.user)
+        self.bot.user_manager.remove_tag(userID, self.tag_name)
+        await self.cog.show_tag_menu(interaction, userID, f"The tag " + self.tag_name + " has been deleted. Your history with this tag still exists. The tag has simply been removed from this menu.")
