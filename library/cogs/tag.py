510907284b62c82db8eb54f3a6a4d1d2dfe5b6b6
marijntepas <marijntepas@gmail.com>
2022-08-16 20:05:51 +0200

    Swapped tag menu over to View with buttons

--- library/cogs/tag.py
+++ library/cogs/tag.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
-from discord import Embed
 from discord.ext.commands import Cog
+from discord.ui import Button, View
 import re
 
 COG_NAME = "tag"
@@ -27,7 +27,7 @@ class Tag(Cog):
         userID = self.bot.user_manager.get_user_id(interaction.user)
 
         if tag_name == "":
-            await self.show_tag_menu(interaction, userID, f"These are your tags. Green tags are active, red tags are not.")
+            await self.show_tag_menu(interaction, userID, f"These are your tags. Green tags are active, red tags are not. Click the tag name to toggle it, and the X to delete a tag.")
         elif not re.match(r"^[a-zA-Z0-9_]*$", tag_name):
             await interaction.response.send_message( f"Tag names may only include alphanumeric characters and underscores. Such as example_tag_2", ephemeral=True)
         elif self.bot.user_manager.has_tag_active(userID, tag_name):
@@ -43,23 +43,22 @@ class Tag(Cog):
             await self.show_tag_menu(interaction, userID, f"The tag " + tag_name + " has been added to your user and turned on.")
 
     async def show_tag_menu(self, interaction, userID, description):
-        embed = Embed(title="Tags", description=description, colour=0x2F3136)
-        embed.set_author(name="Apollo", icon_url="")
-        embed.set_footer(text="___________________________________________________________________________________________")
-
         activeTags = self.bot.user_manager.get_tags_active(userID)
         tags = sorted(activeTags + self.bot.user_manager.get_tags_inactive(userID), key=str.lower)
 
+        view = View()
+        i = 0
         for tag in tags:
             if tag != "":
-                status = ""
-                if tag in activeTags: status = "[On]"
-                else: status = "[Off]"
-                embed.add_field(name="Tag Name", value=tag, inline=True)
-                embed.add_field(name="Status", value=status, inline=True)
-                embed.add_field(name="Delete", value="[X]", inline=True)
+                style=discord.ButtonStyle.red
+                if tag in activeTags: style=discord.ButtonStyle.green
+                button = Button(label=tag, style=style, row=i)
+                view.add_item(button)
+                button2 = Button(label="X", style=style, row=i)
+                view.add_item(button2)
+                i += 1
 
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.response.send_message(description, view=view, ephemeral=True)
 
     @Cog.listener()
     async def on_ready(self):
