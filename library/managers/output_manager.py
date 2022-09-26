b09d3bc7fdd07a4ae591807860dfb766a7019fd9
marijntepas <marijntepas@gmail.com>
2022-09-26 16:36:15 +0200

    Update output_manager.py

--- library/managers/output_manager.py
+++ library/managers/output_manager.py
@@ -214,7 +214,7 @@ class Select_effects(Select):
 
         options = [
             discord.SelectOption(label="upscale_real-esrgan", value="realesrgangan", emoji="↔", description="General purpose upscaling"),
-            discord.SelectOption(label="gfpgan", value="gfpgan", emoji="↔", description="Upscaling with AI face correction"),
+            discord.SelectOption(label="upscale_gfpgan", value="gfpgan", emoji="↔", description="Upscaling with AI face correction"),
             discord.SelectOption(label="style_arcane", value="arcanegan", emoji="🎨", description="Convert into the art style of the animated series Arcane"),
         ]
 
