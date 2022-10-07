af2ed374a65aa2a5e4d6281b80019c3f37c18720
marijntepas <marijntepas@gmail.com>
2022-10-07 21:09:07 +0200

    Removed double underscores from filenames

--- library/managers/computerender_manager.py
+++ library/managers/computerender_manager.py
@@ -63,6 +63,7 @@ class Computerender_manager(object):
                     print('Failed to delete %s. Reason: %s' % (file_path, e))
 
             baseFileName = prompt[:min(len(prompt), 50)].replace(" ", "_").replace("\\", "_").replace("/", "_").replace(":", "_").replace("*", "_").replace("?", "_").replace("\"", "_").replace("<", "_").replace(">", "_").replace("|", "_")
+            while "__" in baseFileName: baseFileName = baseFileName.replace("__", "_")
             filename = f"batch-{baseFileName}-{seed}.png"
             file_path = f"{path}/{filename}"
 
@@ -107,6 +108,7 @@ class Computerender_manager(object):
                         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
                 baseFileName = prompt[:min(len(prompt), 50)].replace(" ", "_").replace("\\", "_").replace("/", "_").replace(":", "_").replace("*", "_").replace("?", "_").replace("\"", "_").replace("<", "_").replace(">", "_").replace("|", "_")
+                while "__" in baseFileName: baseFileName = baseFileName.replace("__", "_")
                 filename = f"variations-{baseFileName}-{seed}.png"
                 file_path = f"{path}/{filename}"
 
@@ -133,6 +135,7 @@ class Computerender_manager(object):
                         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
                 baseFileName = prompt[:min(len(prompt), 50)].replace(" ", "_").replace("\\", "_").replace("/", "_").replace(":", "_").replace("*", "_").replace("?", "_").replace("\"", "_").replace("<", "_").replace(">", "_").replace("|", "_")
+                while "__" in baseFileName: baseFileName = baseFileName.replace("__", "_")
                 filename = f"{baseFileName}-{seed}.png"
                 file_path = f"{path}/{filename}"
 
